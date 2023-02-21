3b6cfddcdd9aec979b59ba3be2bb1b5b46bfcfda
Tyrese <Tyresemalgas76@gmail.com>
2023-02-21 11:05:36 -0800

    9-main.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -14,8 +15,9 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
-	           	      _putchar(k + '0');
+				_putchar(k + '0');
 			}
+
 
 			if (k < 10 && j != 0)
 			{
