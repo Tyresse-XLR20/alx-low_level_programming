55f3548a74191ebadbf3741c1bdb7931ef61529b
Tyresse-XLR20 <Tyresemalgas76@gmail.com>
2023-06-19 06:39:20 -0700

    task 0

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * _putchar - prints a string, followed by a new line, to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-
 void _puts(char *str)
 {
-while (*str != '\0')
-{
-	_putchar(*str++);
+	while (*str != '\0')
+	{
+		putchar(*str++);
+	}
+		putchar('\n');
 }
-	_putchar('\n');
-}
+
